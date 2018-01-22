d05e191a183c908db8cf922353a77e378b400520
USER <user@USER-no-MacBook-Pro.local>
2018-01-23 03:13:32 +0900

    added soft_deletion for user accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         has_many :carts
-         has_many :orders
+         #削除機能をdelete_flagをtrueに変更するに上書き
+         def soft_delete
+         	update_attribute(:delete_flag, true)
+         end
+
+         # ensure user account is active
+         def active_for_authentication?
+         	super && !deleted_at
+         end
+
+		# provide a custom message for a deleted account
+		def inactive_message
+			!deleted_at ? super : :deleted_account
+		end
+
+        has_many :carts
+        has_many :orders
 end
