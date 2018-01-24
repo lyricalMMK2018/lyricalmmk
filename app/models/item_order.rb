d4df3433f93cd390b48ce782242212701c9a5bb4
USER <user@USER-no-MacBook-Pro.local>
2018-01-24 18:27:23 +0900

    installed enum_help

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,5 +1,5 @@
 class ItemOrder < ApplicationRecord
 	belongs_to :order
 	belongs_to :item
-	enum delivery_status: { purchased: 0, ready_for_delivery: 1, departured: 2, delivered: 3 }
+	enum delivery_status: { purchased: 0, ready: 1, departed: 2, delivered: 3 }
 end
