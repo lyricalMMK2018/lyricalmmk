bccc16eec99fa28b9fe36cbc3173ee93383a0915
USER <user@USER-no-MacBook-Pro.local>
2018-01-27 01:49:33 +0900

    Removed unnecessary Admin routings and added missing search routings for items

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
   devise_for :users, :controllers => { :registrations => 'users/registrations'}
   root :to => 'items#index'
   get '/:id' => 'items#show'
   get '/myPage' => 'users#show'
   get '/cart' => 'carts#index'
   get '/purchase_history' => 'orders#index'
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :show] do
+    collection do
+      match 'search' => 'item#search', via: [:get, :post], as: :search
+    end
+  end
   resources :users, only: :show
   resources :carts, only: [:index, :create, :update, :destroy]
   resources :orders, only: [:index, :create]
