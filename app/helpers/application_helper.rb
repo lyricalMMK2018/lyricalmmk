f6d932e010d366016cbd3875c66d2604933b5e56
USER <user@USER-no-MacBook-Pro.local>
2018-01-31 21:10:37 +0900

    functional version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+	def sub_total_per_order(x)
+		sub_total = 0
+		x.item_orders.each do |item_order|
+			y = item_order.saved_price * item_order.quantity
+			sub_total += y
+		end
+		return sub_total
+	end
+
+	def sub_total_each_carts
+		sub_total = 0
+		current_user.carts.each do |cart|
+			y = cart.item.price * cart.quantity
+			sub_total += y
+		end
+		return sub_total
+	end
+
 end
