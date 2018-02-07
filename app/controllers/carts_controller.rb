a6e9affab12e2b9bd284b117a72771452d8ad78e
USER <user@USER-no-MacBook-Pro.local>
2018-02-07 20:50:06 +0900

    rewrote cart-create check if added item already exist

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,20 +8,36 @@ class CartsController < ApplicationController
 	def create
 		#'送信された情報を元にカートの項目を一つ作成'
 		cart = Cart.new(cart_params)
-		#'カートのDBが持ちたいユーザーIDを送信データに加える'
-		cart.user_id = current_user.id
-		#'数量が0以下なら'
-		if cart.quantity <= 0
-			#'itemのindexにリダイレクト'
-			redirect_to item_path(cart.item), alert: 'The quantity must be greater than 0'
-		#商品の個数が在庫より多い場合
-		elsif cart.quantity > cart.item.stock
-			reirect_to item_path(cart.item), alert: 'Please pick items fewer than our stock'
+		#カートに入れようとしているアイテムがすでに存在しているか
+		if current_user.carts.any? {|current_cart| current_cart.item_id = cart.item_id}
+			#ユーザーIDと商品IDが一致するレコードを取得
+			current_cart = Cart.find_by(user_id: current_user.id, item_id: cart.item_id)
+			#存在しているなら、quantityを追加する
+			current_cart.quantity += cart.quantity
+			#在庫数チェック
+			if current_cart.quantity > current_cart.item.stock
+				redirect_to item_path(current_cart.item), alert: 'Please pick items fewer than our stock'
+			else
+				#セーブ&リダイレクト
+				current_cart.save
+				redirect_to carts_path
+			end
 		else
-			#'カート一件のデータを保存'
-			cart.save
-			#'カートのindexにリダイレクト'
-			redirect_to carts_path
+			#'カートのDBが持ちたいユーザーIDを送信データに加える'
+			cart.user_id = current_user.id
+			#'数量が0以下なら'
+			if cart.quantity <= 0
+				#'itemのindexにリダイレクト'
+				redirect_to item_path(cart.item), alert: 'The quantity must be greater than 0'
+			#商品の個数が在庫より多い場合
+			elsif cart.quantity > cart.item.stock
+				reirect_to item_path(cart.item), alert: 'Please pick items fewer than our stock'
+			else
+				#'カート一件のデータを保存'
+				cart.save
+				#'カートのindexにリダイレクト'
+				redirect_to carts_path
+			end
 		end
 	end
 
